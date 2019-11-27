ed1562cfab02215fac822bfb8371bb5759bb71b2
ms03831 <ms03831@st.habib.edu.pk>
2019-11-27 11:24:36 +0500

    api

---
+++ Assignment4APITemplate/Assignment4APITemplate/Models/TaxComputation.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Assignment4APITemplate.Models
+{
+    public class TaxComputation
+    {
+        public decimal TotalSalary { get; set; }
+        public decimal TotalTaxLiability { get; set; }
+        public decimal TaxPaid { get; set; }
+        public int RemainingPeriods { get; set; }
+        public decimal TaxPerMonth { get; set; }
+    }
+}
